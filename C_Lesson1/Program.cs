c3ab30ddfc79f431a322f3b49d4246d5f8dc9366
PUM SUAN KHAI <josephkhaipi@gmail.com>
2019-04-18 19:39:43 +1000

    C# Lesson1

---
+++ C_Lesson1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Dam maw?");
+            Console.ReadLine();
+
+            Console.WriteLine("Na min at in!");
+            string min = Console.ReadLine();
+
+            Console.WriteLine("Na suah kum at in!");
+            int suahkum = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Tu kum bang kum hiam?");
+            int tukum = Convert.ToInt16(Console.ReadLine());
+
+            int kumphazah = tukum - suahkum;
+
+
+
+            Console.WriteLine(min + " " + "pen kum " + " " + kumphazah + " " + "pha hi.");
+            Console.ReadLine();
+      
+        }
+    }
+}
